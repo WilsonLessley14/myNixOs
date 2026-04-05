f6c45f5b948139bae2bff109f2daa2b5c1ae8d2d
WilsonLessley14 <wilsonlessley14@gmail.com>
2026-04-05 18:59:12 -0400

    feat: add fastfetch

--- modules/hosts/t470/configuration.nix
+++ modules/hosts/t470/configuration.nix
@@ -64,7 +64,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
-  #  wget
+    fastfetch
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
