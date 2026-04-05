96b64d3c5443a00d9adeebbd658f9ce925af31f0
WilsonLessley14 <wilsonlessley14@gmail.com>
2026-04-05 16:10:19 -0400

    feat: update niri keybinds

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -23,14 +23,35 @@
 	  };
 	};
 
-	layout.gaps = 5;
+	layout = {
+	  gaps = 5;
+
+	  focus-ring = {
+
+	    width = 1.5;
+
+	  };
+	};
 
 	binds = {
 	 "Mod+S".spawn-sh =
 	   "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
 	 "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
-	 "Mod+Q".close-window = null;
-	};
+	 "Mod+Q".close-window = _: {};
+
+	 "Ctrl+L".focus-column-right-or-first = _: {};
+	 "Ctrl+H".focus-column-left-or-last = _: {};
+	 "Ctrl+K".focus-workspace-up = _: {};
+	 "Ctrl+J".focus-workspace-down = _: {};
+
+         "Ctrl+Shift+L".move-column-right = _: {};
+	 "Ctrl+Shift+H".move-column-left = _: {};
+	 "Ctrl+Shift+K".move-workspace-up = _: {};
+	 "Ctrl+Shift+J".move-workspace-down = _: {};
+
+         "Ctrl+D".toggle-window-floating = _: {};
+	 "Ctrl+T".switch-focus-between-floating-and-tiling = _: {};
+        };
       };
     };
   };
