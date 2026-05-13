feadfe70f7cf941a2cbdbfd2a985e369e533be1a
WilsonLessley14 <wilsonlessley14@gmail.com>
2026-05-13 19:18:31 -0400

    chore(p330, vps): cleanup configuration files. formatting, simplify, etc
    also add wilson user (copy/paste from p330) to vpc

--- modules/hosts/hetzner-vpc/configuration.nix
+++ modules/hosts/hetzner-vpc/configuration.nix
@@ -1,39 +1,52 @@
 { self, inputs, ... }: {
-flake.nixosModules.vpcConfiguration = { config, pkgs, ... }: {
-  imports = [
-    self.nixosModules.vpcHardware
-    self.nixosModules.ghostty
+  flake.nixosModules.vpcConfiguration = { config, pkgs, ... }: {
+    imports = [
+      self.nixosModules.vpcHardware
+      self.nixosModules.ghostty
+      self.nixosModules.neovim
+      inputs.nvf.nixosModules.default
+    ];
 
-  ];
+    nix.settings.experimental-features = [ "flakes" "nix-command" ];
 
-  nix.settings.experimental-features = [ "flakes" "nix-command" ];
+    environment.systemPackages = with pkgs; [
+      git
+    ];
+    
+    users.users.wilson = {
+      isNormalUser = true;
+      description = "wilson";
+      extraGroups = [ "networkmanager" "wheel" "video" "input" ];
+      packages = with pkgs; [];
+      openssh.authorizedKeys.keys = [
+        #m1 mac
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeLEdYfYIu54pjyFta39azix0QHg3YgzsYiVJDf+fug wilsonlessley14@gmail.com"
+        #t470
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILImCZrZJXzU3RkmuUNaq4Jo2nTE2oj0lfL8dAItP6aE wilsonlessley14@gmail.com"
+      ];
+    };
 
-  environment.systemPackages = with pkgs; [
-    git
-    neovim
-  ];
+    networking.wireguard.interfaces.wg0 = {
+      ips = [ "10.0.0.1/24" ];
+      listenPort = 51820;
+      privateKeyFile = "/etc/wireguard/private.key";
+      peers = [{
+        publicKey = "urVgKnvbWGnyrAihi2IROiK88BX/4YG2vFIGXGa1WSc=";
+        allowedIPs = [ "10.0.0.2/32" ];
+      }];
+      firewall.allowedUDPPorts = [ 51820 ];
+      hostName = "ubuntu-2gb-hil-1";
+      domain = "";
+    };
 
-  networking.wireguard.interfaces.wg0 = {
-    ips = [ "10.0.0.1/24" ];
-    listenPort = 51820;
-    privateKeyFile = "/etc/wireguard/private.key";
-    peers = [{
-      publicKey = "urVgKnvbWGnyrAihi2IROiK88BX/4YG2vFIGXGa1WSc=";
-      allowedIPs = [ "10.0.0.2/32" ];
-    }];
+    # Workaround for https://github.com/NixOS/nix/issues/8502
+    services.logrotate.checkConfig = false;
+
+    boot.tmp.cleanOnBoot = true;
+    zramSwap.enable = true;
+    services.openssh.enable = true;
+    users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeLEdYfYIu54pjyFta39azix0QHg3YgzsYiVJDf+fug'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILImCZrZJXzU3RkmuUNaq4Jo2nTE2oj0lfL8dAItP6aE'' ];
+    system.stateVersion = "23.11";
+    
   };
-  networking.firewall.allowedUDPPorts = [ 51820 ];
-
-  # Workaround for https://github.com/NixOS/nix/issues/8502
-  services.logrotate.checkConfig = false;
-
-  boot.tmp.cleanOnBoot = true;
-  zramSwap.enable = true;
-  networking.hostName = "ubuntu-2gb-hil-1";
-  networking.domain = "";
-  services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeLEdYfYIu54pjyFta39azix0QHg3YgzsYiVJDf+fug'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILImCZrZJXzU3RkmuUNaq4Jo2nTE2oj0lfL8dAItP6aE'' ];
-  system.stateVersion = "23.11";
-  
-};
 }
--- modules/hosts/p330/configuration.nix
+++ modules/hosts/p330/configuration.nix
@@ -1,148 +1,111 @@
 {self, inputs, ... }: {
-flake.nixosModules.p330Configuration = { config, pkgs, ... }:
+  flake.nixosModules.p330Configuration = { config, pkgs, ... }: {
+    imports =
+      [ # Include the results of the hardware scan.
+        self.nixosModules.p330Hardware
+        self.nixosModules.neovim
+        self.nixosModules.ghostty
+        self.nixosModules.keyboard
+        inputs.nvf.nixosModules.default
+        self.nixosModules.jellyfin
+      ];
+    
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      self.nixosModules.p330Hardware
-      self.nixosModules.neovim
-      self.nixosModules.ghostty
-      self.nixosModules.keyboard
-      inputs.nvf.nixosModules.default
-      self.nixosModules.jellyfin
-    ];
-  
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "wilsons-thinkstation"; # Define your hostname.
-
-  networking = {
-
-    interfaces.wlp3s0.ipv4.addresses = [{
-      address = "10.67.77.253";
-      prefixLength = 20;
-    }];
-    defaultGateway = {
-      address = "10.67.64.1";
-      interface = "wlp3s0";
+    # Bootloader.
+    boot.loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
     };
-    nameservers = [ "8.8.8.8" ];
-  };
 
-  # Enable networking
-  networking.networkmanager = {
-      enable = true;
-      wifi.macAddress = "permanent";
-  };
+    networking = {
 
+      hostName = "wilsons-thinkstation"; # Define your hostname.
 
-  networking.wireguard.interfaces.wg0 = {
-    ips = [ "10.0.0.2/24" ];
-    privateKeyFile = "/etc/wireguard/private.key";
-    peers = [{
-      publicKey = "S+dcbRQ/SkOReoF5BZGMYvlp2MdJjw91tKucsghRWQk=";
-      allowedIPs = [ "10.0.0.0/24" ];
-      endpoint = "5.78.214.60:51820";
-      persistentKeepalive = 25;
-    }];
-  };
+      interfaces.wlp3s0.ipv4.addresses = [{
+        address = "10.67.77.253";
+        prefixLength = 20;
+      }];
 
-  # Set your time zone.
-  time.timeZone = "America/New_York";
+      defaultGateway = {
+        address = "10.67.64.1";
+        interface = "wlp3s0";
+      };
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+      nameservers = [ "8.8.8.8" ];
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+      # Enable networking
+      networkmanager = {
+        enable = true;
+        wifi.macAddress = "permanent";
+      };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "colemak";
-    options = "caps:ctrl_modifier";
-  };
+      wireguard.interfaces.wg0 = {
+        ips = [ "10.0.0.2/24" ];
+        privateKeyFile = "/etc/wireguard/private.key";
+        peers = [{
+          publicKey = "S+dcbRQ/SkOReoF5BZGMYvlp2MdJjw91tKucsghRWQk=";
+          allowedIPs = [ "10.0.0.0/24" ];
+          endpoint = "5.78.214.60:51820";
+          persistentKeepalive = 25;
+        }];
+      };
 
-  environment.variables = with config.services.xserver; {
-    XKB_DEFAULT_LAYOUT = xkb.layout;
-    XKB_DEFAULT_VARIANT = xkb.variant;
-    XKB_DEFAULT_OPTIONS = xkb.options;
-  };
-
-  console.useXkbConfig = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.wilson = {
-    isNormalUser = true;
-    description = "wilson";
-    extraGroups = [ "networkmanager" "wheel" "video" "input" ];
-    packages = with pkgs; [];
-    openssh.authorizedKeys.keys = [
-      #m1 mac
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeLEdYfYIu54pjyFta39azix0QHg3YgzsYiVJDf+fug wilsonlessley14@gmail.com"
-      #t470
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILImCZrZJXzU3RkmuUNaq4Jo2nTE2oj0lfL8dAItP6aE wilsonlessley14@gmail.com"
-    ];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-     git
-     neovim
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-      KbdInteractiveAuthentication = false;
     };
+
+    # Set your time zone.
+    time.timeZone = "America/New_York";
+
+    # Select internationalisation properties.
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+
+      extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+      };
+    };
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.wilson = {
+      isNormalUser = true;
+      description = "wilson";
+      extraGroups = [ "networkmanager" "wheel" "video" "input" ];
+      packages = with pkgs; [];
+      openssh.authorizedKeys.keys = [
+        #m1 mac
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeLEdYfYIu54pjyFta39azix0QHg3YgzsYiVJDf+fug wilsonlessley14@gmail.com"
+        #t470
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILImCZrZJXzU3RkmuUNaq4Jo2nTE2oj0lfL8dAItP6aE wilsonlessley14@gmail.com"
+      ];
+    };
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs; [
+       git
+    ];
+
+    # Enable the OpenSSH daemon.
+    services = {
+      openssh = {
+        enable = true;
+        settings = {
+          PasswordAuthentication = false;
+          PermitRootLogin = "no";
+          KbdInteractiveAuthentication = false;
+        };
+      };
+    };
+
+    system.stateVersion = "25.11"; # Did you read the comment?
   };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
-};
 }
